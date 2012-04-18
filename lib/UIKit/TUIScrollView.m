033967332a6824aa1bbcb2d80db9abc820cd8fbb
Michael Villar <me@michaelvillar.com>
2012-04-18 17:09:42 +0200

    Fixed bug when scrollingtorect

--- lib/UIKit/TUIScrollView.m
+++ lib/UIKit/TUIScrollView.m
@@ -835,16 +835,16 @@ static float clampBounce(float x) {
 	CGRect visible = self.visibleRect;
 	if(rect.origin.y < visible.origin.y) {
 		// scroll down, have rect be flush with bottom of visible view
-		[self setContentOffset:CGPointMake(0, -rect.origin.y) animated:animated];
+		[self setContentOffset:CGPointMake(self.contentOffset.x, -rect.origin.y) animated:animated];
 	} else if(rect.origin.y + rect.size.height > visible.origin.y + visible.size.height) {
 		// scroll up, rect to be flush with top of view
-		[self setContentOffset:CGPointMake(0, -rect.origin.y + visible.size.height - rect.size.height) animated:animated];
+		[self setContentOffset:CGPointMake(self.contentOffset.x, -rect.origin.y + visible.size.height - rect.size.height) animated:animated];
 	} else if(rect.origin.x < visible.origin.x) {
     // scroll right
-    [self setContentOffset:CGPointMake(-rect.origin.x, 0) animated:animated];
+    [self setContentOffset:CGPointMake(-rect.origin.x, self.contentOffset.y) animated:animated];
   } else if(rect.origin.x + rect.size.width > visible.origin.x + visible.size.width) {
     // scroll right
-    [self setContentOffset:CGPointMake(-rect.origin.x + visible.size.width - rect.size.width, 0) animated:animated];
+    [self setContentOffset:CGPointMake(-rect.origin.x + visible.size.width - rect.size.width, self.contentOffset.y) animated:animated];
   }
 	[self.nsView invalidateHoverForView:self];
 }
